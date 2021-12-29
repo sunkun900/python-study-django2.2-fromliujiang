fa8925764e9cad76d255310ba2787e1d3d7ee0a3
HAM <zhangsan@qq.com>
2021-12-29 20:34:54 +0800

    django2.1 练习完成

--- login/views.py
+++ login/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-from django.shortcuts import  HttpResponse
+from django.shortcuts import HttpResponse
+from login import models
+
 # Create your views here.
+# def index(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         print(username, password)
+#     return render(request, 'index.html')
+user_list = []
+
+
 def index(request):
-    return  HttpResponse("HELLO WORD")
+    # return  HttpResponse("HELLO WORD")
+    # return  render(request,"index.html")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # print(username, password)
+        # temp = {'user': username, 'pwd': password}
+        # user_list.append(temp)
+        models.UserInfo.objects.create(user=username, pwd=password)
+    # 读取所有
+    user_list = models.UserInfo.objects.all()
+    return render(request, 'index.html', {'data': user_list})
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -114,8 +115,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
