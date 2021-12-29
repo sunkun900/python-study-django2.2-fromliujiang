337e077e929242f5b4786e66d701e81db6cfe0c2
HAM <zhangsan@qq.com>
2021-12-29 20:34:54 +0800

    updata index.html

--- login/views.py
+++ login/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.shortcuts import  HttpResponse
+from django.shortcuts import HttpResponse
+
 # Create your views here.
+user_list = []
+
+
 def index(request):
-    return  HttpResponse("HELLO WORD")
+    # return  HttpResponse("HELLO WORD")
+    # return  render(request,"index.html")
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        temp = {'user': username, 'pwd': password}
+        user_list.append(temp)
+    return render(request, 'index.html', {'data:user_list'})
--- mysite/settings.py
+++ mysite/settings.py
@@ -114,8 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
